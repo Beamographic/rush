f937561381122091bd907f4f7975a81b346718b9
Shane Woolcock <samahnub@gmail.com>
2020-05-29 21:58:45 +0930

    Use default hit windows for now

--- osu.Game.Rulesets.Rush/Scoring/RushHitWindows.cs
+++ osu.Game.Rulesets.Rush/Scoring/RushHitWindows.cs
@@ -7,26 +7,5 @@ namespace osu.Game.Rulesets.Rush.Scoring
 {
     public class RushHitWindows : HitWindows
     {
-        private static readonly DifficultyRange[] rush_ranges =
-        {
-            new DifficultyRange(HitResult.Great, 50, 35, 20),
-            new DifficultyRange(HitResult.Good, 120, 80, 50),
-            new DifficultyRange(HitResult.Miss, 135, 95, 70),
-        };
-
-        public override bool IsHitResultAllowed(HitResult result)
-        {
-            switch (result)
-            {
-                case HitResult.Great:
-                case HitResult.Good:
-                case HitResult.Miss:
-                    return true;
-            }
-
-            return false;
-        }
-
-        protected override DifficultyRange[] GetRanges() => rush_ranges;
     }
 }
