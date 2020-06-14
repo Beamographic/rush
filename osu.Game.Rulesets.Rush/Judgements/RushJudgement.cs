9567173e10db97c14f1893fdadba968891b2d63e
Salman Ahmed <email@iisalman.me>
2020-06-14 05:51:50 +0300

    Slightly refactor the health processor for health judgement to be entirely defined by the hit object
    Something I saw and didn't want to let it stay as-is.

--- osu.Game.Rulesets.Rush/Judgements/RushJudgement.cs
+++ osu.Game.Rulesets.Rush/Judgements/RushJudgement.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Shane Woolcock. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Game.Rulesets.Judgements;
 using osu.Game.Rulesets.Scoring;
 
@@ -16,6 +17,20 @@ namespace osu.Game.Rulesets.Rush.Judgements
                 _ => 0
             };
 
-        public virtual double HealthPoints => -10;
+        /// <summary>
+        /// Retrieves the numeric health point increase of a <see cref="RushJudgementResult"/>.
+        /// </summary>
+        /// <param name="result">The <see cref="RushJudgementResult"/> to find the numeric health points increase for.</param>
+        public double HealthPointIncreaseFor(RushJudgementResult result) => HealthPointIncreaseFor(result.Type, result.PlayerCollided);
+
+        /// <summary>
+        /// Retrieves the numeric health point increase of a <see cref="HitResult"/> and whether the player collided.
+        /// </summary>
+        /// <param name="result">The <see cref="HitResult"/> to find the numeric health points increase for.</param>
+        /// <param name="playerCollided">Whether the player collided with the corresponding hit object at the point of judgement.</param>
+        /// <returns>The numeric health points increase of <paramref name="result"/> and <paramref name="playerCollided"/>.</returns>
+        protected virtual double HealthPointIncreaseFor(HitResult result, bool playerCollided) => 0.0;
+
+        protected sealed override double HealthIncreaseFor(HitResult result) => throw new NotSupportedException($"Use the rush-specific version: {nameof(HealthPointIncreaseFor)}");
     }
 }
---
+++ osu.Game.Rulesets.Rush/Judgements/RushJudgementResult.cs
@@ -0,0 +1,33 @@
+// Copyright (c) Shane Woolcock. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Rush.Objects;
+
+namespace osu.Game.Rulesets.Rush.Judgements
+{
+    public class RushJudgementResult : JudgementResult
+    {
+        /// <summary>
+        /// The <see cref="RushHitObject"/> that was judged.
+        /// </summary>
+        public new RushHitObject HitObject => (RushHitObject)base.HitObject;
+
+        /// <summary>
+        /// The judgement which this <see cref="RushJudgementResult"/> applies for.
+        /// </summary>
+        public new RushJudgement Judgement => (RushJudgement)base.Judgement;
+
+        /// <summary>
+        /// Whether the player has collided with the corresponding hit object at the point of judgement.
+        /// </summary>
+        public bool PlayerCollided;
+
+        public RushJudgementResult(RushHitObject hitObject, RushJudgement judgement)
+            : base(hitObject, judgement)
+        {
+        }
+
+        public override string ToString() => $"{Type} (Score:{Judgement.NumericResultFor(this)} HP:{Judgement.HealthPointIncreaseFor(this)} {Judgement})";
+    }
+}
