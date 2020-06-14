8e0aea0fd42a3bfd4a670ebeac8bba5768ab712b
Salman Ahmed <email@iisalman.me>
2020-06-14 05:53:05 +0300

    Add usable collision-damaging judgement

---
+++ osu.Game.Rulesets.Rush/Judgements/CollisionDamagingJudgement.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Shane Woolcock. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.Scoring;
+
+namespace osu.Game.Rulesets.Rush.Judgements
+{
+    /// <summary>
+    /// A judgement that decreases the player health points when
+    /// the player collides with the hit object without hitting it.
+    /// </summary>
+    public class CollisionDamagingJudgement : RushJudgement
+    {
+        protected override double HealthPointIncreaseFor(HitResult result, bool playerCollided) =>
+            result switch
+            {
+                HitResult.Miss when playerCollided => -10.0,
+                _ => 0.0,
+            };
+    }
+}
--- osu.Game.Rulesets.Rush/Objects/DualHitPart.cs
+++ osu.Game.Rulesets.Rush/Objects/DualHitPart.cs
@@ -1,9 +1,13 @@
 // Copyright (c) Shane Woolcock. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Rush.Judgements;
+
 namespace osu.Game.Rulesets.Rush.Objects
 {
     public class DualHitPart : LanedHit
     {
+        public override Judgement CreateJudgement() => new CollisionDamagingJudgement();
     }
 }
