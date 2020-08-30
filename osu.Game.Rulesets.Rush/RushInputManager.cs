c786171ece0a458ee1aaa27652d7573757f543c3
Royce551 <poohwie2005@gmail.com>
2020-08-29 22:11:26 -0500

    Use enum extensions

--- osu.Game.Rulesets.Rush/RushInputManager.cs
+++ osu.Game.Rulesets.Rush/RushInputManager.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using osu.Framework.Input.Bindings;
+using osu.Game.Rulesets.Rush.Objects;
 using osu.Game.Rulesets.UI;
 
 namespace osu.Game.Rulesets.Rush
@@ -46,5 +47,22 @@ namespace osu.Game.Rulesets.Rush
 
         [Description("Air (Quaternary)")]
         AirQuaternary
+    }
+
+    public static class RushActionExtensions
+    {
+        public static LanedHitLane Lane(this RushAction action) =>
+            action switch
+            {
+                RushAction.GroundPrimary => LanedHitLane.Ground,
+                RushAction.GroundSecondary => LanedHitLane.Ground,
+                RushAction.GroundTertiary => LanedHitLane.Ground,
+                RushAction.GroundQuaternary => LanedHitLane.Ground,
+                RushAction.AirPrimary => LanedHitLane.Air,
+                RushAction.AirSecondary => LanedHitLane.Air,
+                RushAction.AirTertiary => LanedHitLane.Air,
+                RushAction.AirQuaternary => LanedHitLane.Air,
+                _ => LanedHitLane.Ground
+            };
     }
 }
