63785f46d6b0afbb7af70705c5258f87236f158d
Salman Ahmed <frenzibyte@gmail.com>
2021-04-10 18:56:51 +0300

    Add rush! replay frame conversion parity test

---
+++ osu.Game.Rulesets.Rush.Tests/Replay/RushReplayFrameTest.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Shane Woolcock. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using NUnit.Framework;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Rush.Replays;
+
+namespace osu.Game.Rulesets.Rush.Tests.Replay
+{
+    [TestFixture]
+    public class RushReplayFrameTest
+    {
+        [TestCase]
+        [TestCase(RushAction.AirPrimary, RushAction.GroundPrimary)]
+        [TestCase(RushAction.AirPrimary, RushAction.AirSecondary, RushAction.GroundQuaternary)]
+        [TestCase(RushAction.GroundPrimary, RushAction.GroundSecondary)]
+        [TestCase(RushAction.AirPrimary, RushAction.AirSecondary, RushAction.AirTertiary, RushAction.AirQuaternary, RushAction.GroundPrimary, RushAction.GroundSecondary, RushAction.GroundTertiary, RushAction.GroundTertiary)]
+        public void TestParity(params RushAction[] actions)
+        {
+            var originalFrame = (RushReplayFrame)new RushRuleset().CreateConvertibleReplayFrame();
+            var resultFrame = (RushReplayFrame)new RushRuleset().CreateConvertibleReplayFrame();
+
+            resultFrame.FromLegacy(originalFrame.ToLegacy(new Beatmap()), new Beatmap());
+
+            Assert.That(resultFrame.Actions, Is.EquivalentTo(originalFrame.Actions));
+        }
+    }
+}
