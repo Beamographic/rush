b27428bc41afd8b89479e5bce86ee07877b8a6b3
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-06-08 20:14:19 +0200

    Resolve breaking changes

--- osu.Game.Rulesets.Rush/RushDifficultyCalculator.cs
+++ osu.Game.Rulesets.Rush/RushDifficultyCalculator.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Shane Woolcock. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Beatmaps;
@@ -25,6 +26,6 @@ namespace osu.Game.Rulesets.Rush
 
         protected override IEnumerable<DifficultyHitObject> CreateDifficultyHitObjects(IBeatmap beatmap, double clockRate) => Enumerable.Empty<DifficultyHitObject>();
 
-        protected override Skill[] CreateSkills(IBeatmap beatmap, Mod[] mods) => new Skill[0];
+        protected override Skill[] CreateSkills(IBeatmap beatmap, Mod[] mods, double clockRate) => Array.Empty<Skill>();
     }
 }
