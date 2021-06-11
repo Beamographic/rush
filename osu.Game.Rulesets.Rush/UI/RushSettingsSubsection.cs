caf0d69f99969903fc4b40381c0c151e6373caa1
Derrick Timmermans <derrick.timmermans@outlook.com>
2021-06-11 13:01:14 +0200

    Remove unneeded null check

--- osu.Game.Rulesets.Rush/UI/RushSettingsSubsection.cs
+++ osu.Game.Rulesets.Rush/UI/RushSettingsSubsection.cs
@@ -22,10 +22,6 @@ namespace osu.Game.Rulesets.Rush.UI
         {
             var config = (RushRulesetConfigManager)Config;
 
-            // for an odd reason, Config seems to be passed as null when creating it. doesnt even get called...
-            if (config == null)
-                return;
-
             Children = new Drawable[]
             {
                 new SettingsCheckbox
