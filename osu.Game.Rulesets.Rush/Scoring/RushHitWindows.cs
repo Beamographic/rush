cd6afa8dc0127cf332e1633b899a96f2c22e6ffe
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-07-10 21:28:07 +0200

    Bump osu and apply HitWindow changes

--- osu.Game.Rulesets.Rush/Scoring/RushHitWindows.cs
+++ osu.Game.Rulesets.Rush/Scoring/RushHitWindows.cs
@@ -1,18 +1,46 @@
 // Copyright (c) Shane Woolcock. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Rulesets.Rush.Scoring
 {
     public class RushHitWindows : HitWindows
     {
-        protected override DifficultyRange[] GetRanges() => new[]
+        private readonly DifficultyRange greatWindowRange = new DifficultyRange(80, 50, 20);
+        private readonly DifficultyRange goodWindowRange = new DifficultyRange(160, 120, 80);
+        private readonly DifficultyRange missWindowRange = new DifficultyRange(200, 180, 160);
+
+        private double great;
+        private double good;
+        private double miss;
+
+        public override void SetDifficulty(double difficulty)
         {
-            new DifficultyRange(HitResult.Great, 80, 50, 20),
-            new DifficultyRange(HitResult.Good, 160, 120, 80),
-            new DifficultyRange(HitResult.Miss, 200, 180, 160),
-        };
+            great = IBeatmapDifficultyInfo.DifficultyRange(difficulty, greatWindowRange);
+            good = IBeatmapDifficultyInfo.DifficultyRange(difficulty, goodWindowRange);
+            miss = IBeatmapDifficultyInfo.DifficultyRange(difficulty, missWindowRange);
+        }
+
+        public override double WindowFor(HitResult result)
+        {
+            switch (result)
+            {
+                case HitResult.Great:
+                    return great;
+
+                case HitResult.Good:
+                    return good;
+
+                case HitResult.Miss:
+                    return miss;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(result), result, null);
+            }
+        }
 
         public override bool IsHitResultAllowed(HitResult result) =>
             result switch
--- osu.Game.Rulesets.Rush/Scoring/SawbladeHitWindows.cs
+++ osu.Game.Rulesets.Rush/Scoring/SawbladeHitWindows.cs
@@ -1,17 +1,40 @@
 // Copyright (c) Shane Woolcock. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Rulesets.Rush.Scoring
 {
     public class SawbladeHitWindows : RushHitWindows
     {
-        protected override DifficultyRange[] GetRanges() => new[]
+        private readonly DifficultyRange greatWindowRange = new DifficultyRange(20, 20, 20);
+        private readonly DifficultyRange missWindowRange = new DifficultyRange(50, 50, 50);
+
+        private double great;
+        private double miss;
+
+        public override void SetDifficulty(double difficulty)
         {
-            new DifficultyRange(HitResult.Great, 20, 20, 20),
-            new DifficultyRange(HitResult.Miss, 50, 50, 50)
-        };
+            great = IBeatmapDifficultyInfo.DifficultyRange(difficulty, greatWindowRange);
+            miss = IBeatmapDifficultyInfo.DifficultyRange(difficulty, missWindowRange);
+        }
+
+        public override double WindowFor(HitResult result)
+        {
+            switch (result)
+            {
+                case HitResult.Great:
+                    return great;
+
+                case HitResult.Miss:
+                    return miss;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(result), result, null);
+            }
+        }
 
         public override bool IsHitResultAllowed(HitResult result) => result == HitResult.Great || result == HitResult.Miss;
     }
