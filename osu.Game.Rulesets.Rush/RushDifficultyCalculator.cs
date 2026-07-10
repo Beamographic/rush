3cb0e74b27efb129ffa2c5690296295c1747537b
Derrick Timmermans <derrick.timmermans@outlook.com>
2026-07-10 23:10:01 +0200

    Update DifficultyCalculator

--- osu.Game.Rulesets.Rush/RushDifficultyCalculator.cs
+++ osu.Game.Rulesets.Rush/RushDifficultyCalculator.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Shane Woolcock. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Difficulty;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
@@ -19,13 +17,12 @@ namespace osu.Game.Rulesets.Rush
         {
         }
 
-        protected override DifficultyAttributes CreateDifficultyAttributes(IBeatmap beatmap, Mod[] mods, Skill[] skills, double clockRate)
-        {
-            return new DifficultyAttributes(mods, 0) { StarRating = beatmap.BeatmapInfo.StarRating };
-        }
 
-        protected override IEnumerable<DifficultyHitObject> CreateDifficultyHitObjects(IBeatmap beatmap, double clockRate) => Enumerable.Empty<DifficultyHitObject>();
+        protected override DifficultyAttributes CreateDifficultyAttributes(IBeatmap beatmap, Mod[] mods, Skill[] skills)
+            => new DifficultyAttributes(mods, 0) { StarRating = beatmap.BeatmapInfo.StarRating };
 
-        protected override Skill[] CreateSkills(IBeatmap beatmap, Mod[] mods, double clockRate) => Array.Empty<Skill>();
+        protected override IEnumerable<DifficultyHitObject> CreateDifficultyHitObjects(IBeatmap beatmap, Mod[] mods) => [];
+
+        protected override Skill[] CreateSkills(IBeatmap beatmap, Mod[] mods) => [];
     }
 }
